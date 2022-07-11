d57b4ae9ba7e9972a54c561af8ac6a2a72d74bed
esteban <estebanfernandez20003024@gmail.com>
2022-07-11 11:06:52 -0300

    agragados

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ else{
  */
 
 /* desafio entregable 1 */
-
+alert("Siguiendo las indicaciones podras calcular el total a pagar.")
 let total = prompt("total a pagar")
 let cuotas = prompt("cuotas a pagar")
 
@@ -87,7 +87,8 @@ else if (cuotas > 12){
     alert("15% de recargo" + " =   $" + total*15/100)
 }
 
+let intereses = total*cuotas/100
 
-let preciofinal = total/cuotas + " + intereses "
+let preciofinal = total/cuotas + intereses 
 
 alert("te quedaria cuotas de $" + preciofinal)
