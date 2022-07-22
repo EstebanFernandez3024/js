9b8c570f284408b43e0d37a53a0de9480491e6f2
esteban <estebanfernandez20003024@gmail.com>
2022-07-22 14:43:54 -0300

    implementando pokemones

--- index.js
+++ index.js
@@ -1,41 +1,26 @@
 
-alert("Siguiendo las indicaciones podras calcular el total a pagar.")
-let total = prompt("total a pagar")
-let cuotas = prompt("cuotas a pagar")
 
-if(cuotas <= 6){
-    alert("5% de recargo" + " =   $ " + total*5/100)
-}
-else if (cuotas = 12){
-    alert("8% de recargo" + " =   $" + total*8/100)
-}
-else if (cuotas > 12){
-    alert("15% de recargo" + " =   $" + total*15/100)
-}
-
-let intereses = total*5/100
-
-let preciofinal = total/cuotas + intereses 
-
-alert("te quedaria cuotas de $" + preciofinal)
-
-alert("precio final $" + (total+intereses))
+container.innerHTML = `<p>POKEMONES</p>`
 
 
-class Formulario{
+const tarjetasHtml = POKEMON.pokemon.reduce((acc, elemento, i) => {   
 
-    constructor(nombre,apellido, edad, domicilio, codigoPostal, ciudad, pais, estadoCivil){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.edad=edad;
-        this.domicilio=domicilio;
-        this.codigoPostal=codigoPostal;
-        this.ciudad=ciudad;
-        this.pais=pais;
-        this.estadoCivil=estadoCivil;
-    }
-    
-}
+        return acc = acc + `    
+            <div class="tarjeta">
+                <div class="img-container">
+                    <img src=${elemento.img} alt=${elemento.name}>
+                </div>   
+                <p>
+                    ${elemento.name}
+                </p>  
+            </div>
+        `       
+},"")
 
-const formularios = new Formulario("Esteban", "Fernandez", 33, "Rivadavia 3024", 1824, "Buenos Aires","Argentina","solero")
- console.log(formularios);
+console.log(tarjetasHtml)
+
+const contenedorTarjetas = document.querySelector(".contenedor-tarjetas")
+
+console.log(contenedorTarjetas)
+
+contenedorTarjetas.innerHTML = tarjetasHtml
